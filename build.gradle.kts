cf9556186d7546ee34aead646bcd9e721fa2de96
Jonathan Schneider <jkschneider@gmail.com>
2021-09-19 21:50:41 -0700

    Update build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,40 +1,60 @@
-import nebula.plugin.info.InfoBrokerPlugin
 import nl.javadude.gradle.plugins.license.LicenseExtension
 import java.util.*
 
-buildscript {
-    repositories {
-        jcenter()
-        gradlePluginPortal()
-    }
-
-    dependencies {
-        classpath("io.spring.gradle:spring-release-plugin:0.20.1")
-
-        constraints {
-            classpath("org.jfrog.buildinfo:build-info-extractor-gradle:4.13.0") {
-                because("Need recent version for Gradle 6+ compatibility")
-            }
-        }
-    }
-}
-
 plugins {
     `java-library`
-    id("org.jetbrains.kotlin.jvm") version "1.4.20"
-    id("io.spring.release") version "0.20.1"
+    `maven-publish`
+    signing
+
+    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    id("nebula.maven-resolved-dependencies") version "17.3.2"
+    id("nebula.release") version "15.3.1"
+    id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
+
+    id("com.github.hierynomus.license") version "0.16.1"
+
+    id("nebula.maven-publish") version "17.3.2"
+    id("nebula.contacts") version "5.1.0"
+    id("nebula.info") version "9.3.0"
+
+    id("nebula.javadoc-jar") version "17.3.2"
+    id("nebula.source-jar") version "17.3.2"
+    id("nebula.maven-apache-license") version "17.3.2"
 }
 
-apply(plugin = "license")
-apply(plugin = "nebula.maven-resolved-dependencies")
-apply(plugin = "nebula.maven-publish")
+apply(plugin = "nebula.publish-verification")
 
-group = "org.openrewrite"
+configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
+    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
+}
+
+group = "org.openrewrite.recipe"
+description = "Enforce logging best practices and migrate between logging frameworks. Automatically."
 
 repositories {
-    mavenLocal()
-    maven { url = uri("https://dl.bintray.com/openrewrite/maven") }
+    if (!project.hasProperty("releasing")) {
+        mavenLocal()
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+        }
+    }
     mavenCentral()
+}
+
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+
+signing {
+    setRequired({
+        !project.version.toString().endsWith("SNAPSHOT") || project.hasProperty("forceSigning")
+    })
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
+    sign(publishing.publications["nebula"])
 }
 
 configurations.all {
@@ -44,10 +64,12 @@ configurations.all {
     }
 }
 
-java {
-    withJavadocJar()
-    withSourcesJar()
+val rewriteVersion = if (project.hasProperty("releasing")) {
+    "latest.release"
+} else {
+    "latest.integration"
 }
+
 
 tasks.withType<Javadoc> {
     exclude("**/RewriteAnnotationProcessor**")
@@ -72,20 +94,10 @@ dependencies {
     testRuntimeOnly("ch.qos.logback:logback-classic:1.0.13")
 }
 
-tasks.named<JavaCompile>("compileJava") {
-    sourceCompatibility = JavaVersion.VERSION_11.toString()
-    targetCompatibility = JavaVersion.VERSION_11.toString()
-
-    options.compilerArgs.clear() // remove `--release 8` set in root gradle build
-    options.compilerArgs.addAll(listOf(
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
-        "--add-exports", "jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED"
-    ))
+tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 tasks.named<Test>("test") {
@@ -93,11 +105,31 @@ tasks.named<Test>("test") {
     jvmArgs = listOf("-XX:+UnlockDiagnosticVMOptions", "-XX:+ShowHiddenFrames")
 }
 
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
+
+    options.isFork = true
+    options.forkOptions.executable = "javac"
+    options.compilerArgs.addAll(listOf("--release", "8"))
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
+
+
+configure<nebula.plugin.contacts.ContactsExtension> {
+    val j = nebula.plugin.contacts.Contact("team@moderne.io")
+    j.moniker("Team Moderne")
+
+    people["team@moderne.io"] = j
+}
+
 configure<LicenseExtension> {
     ext.set("year", Calendar.getInstance().get(Calendar.YEAR))
     skipExistingHeaders = true
     header = project.rootProject.file("gradle/licenseHeader.txt")
     mapping(mapOf("kt" to "SLASHSTAR_STYLE", "java" to "SLASHSTAR_STYLE"))
+    exclude("src/main/resources/*.java")
     strictCheck = true
 }
 
@@ -105,72 +137,6 @@ configure<PublishingExtension> {
     publications {
         named("nebula", MavenPublication::class.java) {
             suppressPomMetadataWarningsFor("runtimeElements")
-
-            pom.withXml {
-                (asElement().getElementsByTagName("dependencies").item(0) as org.w3c.dom.Element).let { dependencies ->
-                    dependencies.getElementsByTagName("dependency").let { dependencyList ->
-                        var i = 0
-                        var length = dependencyList.length
-                        while (i < length) {
-                            (dependencyList.item(i) as org.w3c.dom.Element).let { dependency ->
-                                if ((dependency.getElementsByTagName("scope")
-                                                .item(0) as org.w3c.dom.Element).textContent == "provided") {
-                                    dependencies.removeChild(dependency)
-                                    i--
-                                    length--
-                                }
-                            }
-                            i++
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-tasks.withType<GenerateMavenPom> {
-    doLast {
-        // because pom.withXml adds blank lines
-        destination.writeText(
-                destination.readLines().filter { it.isNotBlank() }.joinToString("\n")
-        )
-    }
-
-    doFirst {
-        val runtimeClasspath = configurations.getByName("runtimeClasspath")
-
-        val gav = { dep: ResolvedDependency ->
-            "${dep.moduleGroup}:${dep.moduleName}:${dep.moduleVersion}"
-        }
-
-        val observedDependencies = TreeSet<ResolvedDependency> { d1, d2 ->
-            gav(d1).compareTo(gav(d2))
-        }
-
-        fun reduceDependenciesAtIndent(indent: Int):
-                (List<String>, ResolvedDependency) -> List<String> =
-                { dependenciesAsList: List<String>, dep: ResolvedDependency ->
-                    dependenciesAsList + listOf(" ".repeat(indent) + dep.module.id.toString()) + (
-                            if (observedDependencies.add(dep)) {
-                                dep.children
-                                        .sortedBy(gav)
-                                        .fold(emptyList(), reduceDependenciesAtIndent(indent + 2))
-                            } else {
-                                // this dependency subtree has already been printed, so skip it
-                                emptyList()
-                            }
-                            )
-                }
-
-        project.plugins.withType<InfoBrokerPlugin> {
-            add("Resolved-Dependencies", runtimeClasspath
-                    .resolvedConfiguration
-                    .lenientConfiguration
-                    .firstLevelModuleDependencies
-                    .sortedBy(gav)
-                    .fold(emptyList(), reduceDependenciesAtIndent(6))
-                    .joinToString("\n", "\n", "\n" + " ".repeat(4)))
         }
     }
 }
