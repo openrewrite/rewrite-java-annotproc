a8b9e6315ab15184e4a5f6ca03d9306d1562dd9f
Jonathan Schneider <jkschneider@gmail.com>
2021-09-19 22:01:48 -0700

    Open modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -140,3 +140,19 @@ configure<PublishingExtension> {
         }
     }
 }
+
+tasks.named<JavaCompile>("compileJava") {
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
+
+    options.compilerArgs.clear() // remove `--release 8` set in root gradle build
+    options.compilerArgs.addAll(listOf(
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+        "--add-exports", "jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED"
+    ))
+}
